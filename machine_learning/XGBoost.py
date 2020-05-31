8c39c32cbd3308811b279516ea721ba4e6b10fee
jbofill10 <juan.bofill10@gmail.com>
2020-05-31 19:28:48 -0400

    Refactor XGBoost

--- machine_learning/XGBoost.py
+++ machine_learning/XGBoost.py
@@ -6,33 +6,33 @@ import os
 import pickle
 
 
-def run_xgboost(x_train, y_train, x_test, y_test):
-    if not os.path.isfile('Data/pickles/xgboost_model'):
-        params = {
-            'n_estimators': [10, 20, 50, 100, 250, 500, 1000],
-            'max_depth': [1, 3, 5],
-            'learning_rate': [0, 0.01, 0.02, 0.03, 0.04, 0.05, 0.07, 0.08, 0.09, 0.1],
-            'reg_alpha': [0, 0.001, 0.1, 0.5, 1],
-            'reg_lambda': [0, 0.001, 0.1, 1],
-            'n_jobs': [-1]
+def train(x_train, y_train):
+    params = {
+        'n_estimators': [500],
+        'max_depth': [1, 3, 5],
+        'learning_rate': [0.09],
+        'reg_alpha': [0],
+        'reg_lambda': [0],
+        'n_jobs': [-1]
 
-        }
+    }
 
-        model = XGBClassifier(tree_method='gpu_hist')
-        grid = GridSearchCV(estimator=model, param_grid=params, cv=5, verbose=3, scoring='accuracy')
+    model = XGBClassifier(tree_method='gpu_hist')
+    grid = GridSearchCV(estimator=model, param_grid=params, cv=5, verbose=3, scoring='accuracy')
 
-        grid.fit(x_train, y_train)
-        print(grid.best_params_)
-        with open('Data/pickles/xgboost_model', 'wb') as file:
-            pickle.dump(grid, file)
-    else:
-        with open('Data/pickles/xgboost_model', 'rb') as file:
-            grid = pickle.load(file)
-    
+    grid.fit(x_train, y_train)
+
+    with open('Data/pickles/xgboost_model', 'wb') as file:
+        pickle.dump(grid, file)
+
+
+def predict(x_test, y_test):
+    with open('Data/pickles/xgboost_model', 'rb') as file:
+        grid = pickle.load(file)
+
     model = grid.best_estimator_
 
     y_pred = model.predict(x_test)
 
     print(confusion_matrix(y_test, y_pred))
     print(accuracy_score(y_test, y_pred))
-
